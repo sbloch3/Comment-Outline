5acf2d2925c6508cd9d34ccd98effcad6167dfd9
sbloch3 <sbloch3@wisc.edu>
2021-11-21 21:22:12 -0600

    Create commentsOutline.py

---
+++ commentsOutline.py
@@ -0,0 +1,16 @@
+
+
+#1) Make a single line comment.
+
+'''Comment here'''
+
+#2) Make a multi-line comment.
+''' please comment here
+thank you'''
+
+
+
+#3) Make an int variable. Then, make a single line comment on the 
+#same line, after the variable.
+x = 5
+'''if x = 5 than y = x '''
